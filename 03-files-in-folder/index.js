64f9739de2c97c042f175a29c6183739887ff981
vadimkonyok <vadim.konyok@mail.ru>
2022-05-26 20:24:18 +0300

    fix: fixed bug with file name display

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,18 +3,18 @@ const path = require("path");
 const stream = path.join(__dirname, "secret-folder");
 const { stdout } = process;
 
-fs.readdir(stream, { withFileTypes: true }, (err, data) => {
+fs.readdir(stream, (err, data) => {
   if (err) throw err;
 
   data.forEach((element) => {
-    if (element.isFile()) {
-      const pathFile = path.join(__dirname, "secret-folder", `${element.name}`);
-      const fileName = element.name.split(".")[0];
-      const extFile = path.extname(element.name).split(".")[1];
-      fs.stat(path.join(pathFile), (err, stats) => {
-        if (err) throw err;
+    const pathFile = path.join(stream, element);
+    const fileName = path.parse(element).name;
+    const extFile = path.extname(element).slice(1);
+    fs.stat(path.join(pathFile), (err, stats) => {
+      if (err) throw err;
+      if (!stats.isDirectory()) {
         stdout.write(`${fileName} - ${extFile} - ${stats.size / 1000} Kb\n`);
-      });
-    }
+      }
+    });
   });
 });
